2617e4825a0330669b19ad09bd0a7496c24131a0
Arindam B <arindam.bagchi@outlook.com>
2024-05-11 00:55:53 +0530

    Simple Bulk Dezipper FInal

---
+++ FolderDezipper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+
+string folderPath = @"";
+
+string[] zipFiles = Directory.GetFiles(folderPath, "*.zip");
+
+
+foreach (string zipFile in zipFiles)
+{
+string fileName = Path.GetFileNameWithoutExtension(zipFile);
+string destinationFolder = Path.Combine(folderPath, fileName);
+
+
+try
+{
+    if (!Directory.Exists(destinationFolder))
+        Directory.CreateDirectory(destinationFolder);
+
+    ZipFile.ExtractToDirectory(zipFile, destinationFolder);
+    Console.WriteLine($"Extracted {zipFile} to {destinationFolder}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error extracting {zipFile}: {ex.Message}");
+}
+}
+
+Console.WriteLine("Extraction complete.");
+
